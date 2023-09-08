21c8e0d3fefd85c94119b2e33ed45c4a440d5137
saehirata <142714849+saehirata@users.noreply.github.com>
2023-09-08 21:06:52 +0900

    before_actionの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
 
   def index
     @prototype = Prototype.all
